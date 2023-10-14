5674d9a66704f2322ce7a84a28c101c2867fa048
arsperger <arsperger@gmail.com>
2023-10-14 11:13:26 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,9 +22,7 @@ BASE_URL = "/accounts"
 
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
-######################################################################
-#  T E S T   C A S E S
-######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -187,8 +185,8 @@ class TestAccountService(TestCase):
             'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
-        #for key, value in headers.items():
-        #    self.assertEqual(response.headers.get(key), value)
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
 
     def test_cors_security(self):
         """It should return a CORS header"""
